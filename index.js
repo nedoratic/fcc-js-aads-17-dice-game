f537facd6b8dced3aa57820dea9481d9d2d339e2
nedoratic <nedoratic@gmail.com>
2024-05-04 20:40:42 +0200

    index.js - rules button event listener added

--- index.js
+++ index.js
@@ -25,3 +25,16 @@ let score = 0;
 let totalScore = 0;
 let round = 1;
 let rolls = 0;
+
+// Rules Button Event Listener
+rulesBtn.addEventListener('click', () => {
+	isModalShowing = !isModalShowing;
+
+	if (isModalShowing) {
+		rulesBtn.textContent = 'Hide Rules';
+		rulesContainer.style.display = 'block';
+	} else {
+		rulesBtn.textContent = 'Show Rules';
+		rulesContainer.style.display = 'none';
+	}
+});

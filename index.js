d7e477230fe7e5c8fcb4e4819c29e06237528af7
nedoratic <nedoratic@gmail.com>
2024-05-06 06:40:37 +0200

    index.js - update stats function added

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
 // DOM Elements Variables
 const listOfAllDice = document.querySelectorAll('.die');
-
 const scoreInputs = document.querySelectorAll('#score-options input');
 const scoreSpans = document.querySelectorAll('#score-options span');
-
 const currentRoundText = document.getElementById('current-round');
 const currentRoundRollsText = document.getElementById('current-round-rolls');
-
 const totalScoreText = document.getElementById('total-score');
-
 const scoreHistory = document.getElementById('score-history');
-
 const rollDiceBtn = document.getElementById('roll-dice-btn');
 const keepScoreBtn = document.getElementById('keep-score-btn');
-
 const rulesContainer = document.querySelector('.rules-container');
-
 const rulesBtn = document.getElementById('rules-btn');
 
 // Game Variables
@@ -26,7 +19,7 @@ let totalScore = 0;
 let round = 1;
 let rolls = 0;
 
-// Roll Dice Function
+// Roll Dice
 const rollDice = () => {
 	diceValuesArr = [];
 
@@ -38,6 +31,12 @@ const rollDice = () => {
 	listOfAllDice.forEach((dice, index) => {
 		dice.textContent = diceValuesArr[index];
 	});
+};
+
+// Update Stats
+const updateStats = () => {
+	currentRoundRollsText.textContent = rolls;
+	currentRoundText.textContent = round;
 };
 
 // Rules Button Event Listener

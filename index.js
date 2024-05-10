ba1b67abeab6047fe318fa4235dd02ed7b3895c2
nedoratic <nedoratic@gmail.com>
2024-05-10 10:21:59 +0200

    index.js - roll dice button event listener edited

--- index.js
+++ index.js
@@ -130,8 +130,10 @@ rollDiceBtn.addEventListener('click', () => {
 		alert('You have made three rolls this round. Please select a score.');
 	} else {
 		rolls++;
+		resetRadioOption();
 		rollDice();
 		updateStats();
+		getHighestDuplicates(diceValuesArr);
 	}
 });
 

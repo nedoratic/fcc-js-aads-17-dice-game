845931fe462819b37975fee7e4bce923d52c3ad5
nedoratic <nedoratic@gmail.com>
2024-05-10 10:23:18 +0200

    index.js - keep score button event listener edited

--- index.js
+++ index.js
@@ -172,6 +172,7 @@ keepScoreBtn.addEventListener('click', () => {
 		if (round > 6) {
 			setTimeout(() => {
 				alert(`Game Over! Your total score is ${totalScore}`);
+				resetGame();
 			}, 500);
 		}
 	} else {

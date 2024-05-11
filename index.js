a46a466fa000c69ecb28ea79d4b04e08d96273b4
nedoratic <nedoratic@gmail.com>
2024-05-11 22:12:01 +0200

    index.js - check for straights function added

--- index.js
+++ index.js
@@ -109,6 +109,26 @@ const detectFullHouse = (arr) => {
 	updateRadioOption(5, 0);
 };
 
+// Check For Straights
+const checkForStraights = (arr) => {
+	const sortedNumbersArr = arr.sort((a, b) => a - b);
+	const uniqueNumbersArr = [...new Set(sortedNumbersArr)];
+	const uniqueNumbersStr = uniqueNumbersArr.join('');
+
+	const smallStraightsArr = ['1234', '2345', '3456'];
+	const largeStraightsArr = ['12345', '23456'];
+
+	if (smallStraightsArr.some((straight) => uniqueNumbersStr.includes(straight))) {
+		updateRadioOption(3, 30);
+	}
+
+	if (largeStraightsArr.includes(uniqueNumbersStr)) {
+		updateRadioOption(4, 40);
+	}
+
+	updateRadioOption(5, 0);
+};
+
 // Reset Radio Option
 const resetRadioOption = () => {
 	scoreInputs.forEach((input) => {
